c41607211662aca08f064495f51c437218c6a7c5
NIRANJAN KUMAR M <37568654+stepin104545@users.noreply.github.com>
2020-09-19 10:27:32 +0530

    Add files via upload

---
+++ c++assignment/set-1/ipaddress/pch.h
@@ -0,0 +1,18 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#include<string>
+
+class IPAddress {
+private:
+	int ipval[4];
+public:
+	IPAddress();
+	IPAddress(int[], int[], int[], int[]);
+	IPAddress(int[]);
+	bool isLoopBack(int[], int[], int[], int[]);
+	std::string getIPClass(int[]);
+	void display();
+};
